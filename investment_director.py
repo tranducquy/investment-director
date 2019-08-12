c098f4b41b31c14d90fe3f7a111db5b42f523947
ryzen <takeyuki.t@gmail.com>
2019-08-13 07:33:03 +0900

    騰落率が安定している銘柄のみ提案

--- investment_director.py
+++ investment_director.py
@@ -28,62 +28,90 @@ if __name__ == '__main__':
     #3ヶ月、1,3,15年のバックテストで利益の出ている銘柄のみ探す
     conn = sqlite3.connect(dbfile)
     c = conn.cursor()
-    today = datetime.today()
+    c.execute("""
+    select
+    max(regist_date)
+    from backtest_result""")
+    max_regist_date = c.fetchone()
+
+    #today = datetime.today()
+    today = datetime.strptime(max_regist_date[0], "%Y-%m-%d")
     start_date_3month = (today - relativedelta(months=3)).strftime("%Y-%m-%d")
     start_date_1year = (today - relativedelta(years=1)).strftime("%Y-%m-%d")
     start_date_3year = (today - relativedelta(years=3)).strftime("%Y-%m-%d")
     start_date_15year = (today - relativedelta(years=15)).strftime("%Y-%m-%d")
     start_date = (today - timedelta(days=30)).strftime("%Y-%m-%d")
     end_date = (today - timedelta(days=1)).strftime("%Y-%m-%d")
+
     c.execute("""
-    select
-     symbol
-    ,strategy
-    ,regist_date
-    ,rate_of_return
-    ,expected_rate
-    ,expected_rate_per_1day
-    ,average_period_per_trade
-    ,win_rate
-    from backtest_result
-    where 
-    (symbol||strategy) in (
-        select 
-         symbol||strategy
-        from backtest_result
-        where (symbol||strategy) in (
-            select
-             symbol||strategy
-            from backtest_result
-            where (symbol||strategy) in (
-                select
-                 symbol||strategy
-                from backtest_result
-                where start_date = ?
-                and end_date = ?
-                and backtest_period > 20*3
-                and rate_of_return > 0
-            )
-            and start_date = ?
-            and end_date = ?
-            and backtest_period > 300*1
-            and rate_of_return > 0
-        )
-        and start_date = ?
-        and end_date = ?
-        and backtest_period > 300*3
-        and rate_of_return > 0
-    ) 
-    and start_date = ?
-    and end_date = ?
-    and backtest_period > 300*15
-    and rate_of_return > 0
-    and regist_date = '2019-08-12'
-    order by rate_of_return desc
-    """, (start_date_3month, end_date
-        , start_date_1year, end_date
+ select
+  m3.symbol
+,m3.strategy
+,m3.regist_date
+,m3.rate_of_return as 騰落率3か月
+,y1.rate_of_return as 騰落率1年
+,y3.rate_of_return as 騰落率3年
+,y15.rate_of_return as 騰落率15年
+,m3.expected_rate as トレード当たりの期待値3か月
+,y1.expected_rate as トレード当たりの期待値1年
+,y3.expected_rate as トレード当たりの期待値3年
+,y15.expected_rate as トレード当たりの期待値15年
+,m3.expected_rate_per_1day as トレード1日当たりの期待値3か月
+,y1.expected_rate_per_1day as トレード1日当たりの期待値1年
+,y3.expected_rate_per_1day as トレード1日当たりの期待値3年
+,y15.expected_rate_per_1day as トレード1日当たりの期待値15年
+,m3.average_period_per_trade as 平均取引期間3か月
+,y1.average_period_per_trade as 平均取引期間1年
+,y3.average_period_per_trade as 平均取引期間3年
+,y15.average_period_per_trade as 平均取引期間15年
+,m3.win_rate as 勝率3か月
+,y1.win_rate as 勝率1年
+,y3.win_rate as 勝率3年
+,y15.win_rate as 勝率15年
+from backtest_result m3
+left outer join (
+select
+ *
+from backtest_result
+where start_date = ?
+and end_date = ?
+and backtest_period > 300*1
+and rate_of_return > 0
+) y1
+on m3.symbol = y1.symbol and m3.strategy = y1.strategy and m3.rate_of_return < y1.rate_of_return
+left outer join (
+select
+ *
+from backtest_result
+where start_date = ?
+and end_date = ?
+and backtest_period > 300*3
+and rate_of_return > 0
+) y3
+on m3.symbol = y3.symbol and m3.strategy = y3.strategy and y1.rate_of_return < y3.rate_of_return
+left outer join (
+select
+ *
+from backtest_result
+where start_date = ?
+and end_date = ?
+and backtest_period > 300*15
+and rate_of_return > 0
+) y15
+on m3.symbol = y15.symbol and m3.strategy = y15.strategy and y3.rate_of_return < y15.rate_of_return
+where m3.start_date = ?
+and m3.end_date = ?
+and m3.rate_of_return > 0
+and m3.rate_of_return < y1.rate_of_return
+and y1.rate_of_return < y3.rate_of_return
+and y3.rate_of_return < y15.rate_of_return
+order by m3.rate_of_return desc
+    """, (
+          start_date_1year, end_date
         , start_date_3year, end_date
-        , start_date_15year, end_date))
+        , start_date_15year, end_date
+        , start_date_3month, end_date
+        ))
     symbols = c.fetchall()
     conn.close()
 
