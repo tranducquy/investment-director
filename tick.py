9ba4f5b1f5842dc040a32da71d022a7f3c1d8bec
1011.mbp2016 <takeyuki.t@gmail.com>
2019-08-16 19:23:23 +0900

    GBP=Xに対応

--- tick.py
+++ tick.py
@@ -1,7 +1,9 @@
 
 
 def get_tick(symbol):
-    if symbol.endswith('=X') == 1 or symbol.startswith('^') == 1: #為替と株式指数
+    if symbol == 'GBP=X': #USD/GBP
+        tick = 0.0001
+    elif symbol.endswith('=X') == 1 or symbol.startswith('^') == 1: #為替と株式指数
         tick = 0.01
     elif symbol.find('-') >= 1: #暗号通貨
         tick = 0.0001
