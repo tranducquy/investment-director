4f49b161343613b42f11a7d072b1754521966396
1011.prodesk.ubuntu <takeyuki.t@gmail.com>
2019-08-23 06:28:01 +0900

    flask

--- www/got_the_nuts.py
+++ www/got_the_nuts.py
@@ -5,6 +5,49 @@ import sqlite3
 app = Flask(__name__)
 
 DATABASE = '/usr/local/investment-director/market-history.db'
+BACKTEST_HISTORY_QUERY = """
+                    select 
+                     bh.symbol
+                    ,ms.name
+                    ,bh.business_date
+                    ,bh.open
+                    ,bh.high
+                    ,bh.low
+                    ,bh.close
+                    ,bh.volume
+                    ,bh.sma
+                    ,bh.upper_sigma1
+                    ,bh.lower_sigma1
+                    ,bh.upper_sigma2
+                    ,bh.lower_sigma2
+                    ,bh.vol_sma
+                    ,bh.vol_upper_sigma1
+                    ,bh.vol_lower_sigma1
+                    ,bh.order_create_date
+                    ,bh.order_type
+                    ,bh.order_vol
+                    ,bh.order_price
+                    ,bh.call_order_date
+                    ,bh.call_order_type
+                    ,bh.call_order_vol
+                    ,bh.call_order_price
+                    ,bh.execution_order_date
+                    ,bh.execution_order_type
+                    ,bh.execution_order_status
+                    ,bh.execution_order_vol
+                    ,bh.execution_order_price
+                    ,bh.position
+                    ,bh.cash
+                    ,bh.pos_vol
+                    ,bh.pos_price
+                    ,bh.total_value
+                    ,bh.profit_value
+                    ,bh.profit_rate
+                    from backtest_history as bh
+                    left outer join m_strategy as ms
+                     on bh.strategy_id = ms.id
+                    where bh.symbol = '{symbol}'
+                    order by business_date"""
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -22,23 +65,31 @@ def query_db(query, args=(), one=False):
 def index():
     return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route("/index")
+def index2():
+    return render_template('index.html')
+
+@app.route('/signal')
+def signal():
+    return 'Investment Signal'
 
 @app.route('/bitmex_xbtusd')
 def bitmex_xbtusd():
-    db = get_db()
-    rv = query_db("select * from backtest_history where symbol = 'XBTUSD'")
-    return render_template('tables.html')
+    rv = query_db(BACKTEST_HISTORY_QUERY.format(symbol='XBTUSD'))
+    content_title = "BitMEX XBTUSD Backtest Data"
+    return render_template('backtest_history_table.html', content_title=content_title, rv=rv)
 
 @app.route('/bitmex_ethusd')
 def bitmex_ethusd():
-    return 'bitmex_ethusd'
+    rv = query_db(BACKTEST_HISTORY_QUERY.format(symbol='ETHUSD'))
+    content_title = "BitMEX ETHUSD Backtest Data"
+    return render_template('backtest_history_table.html', content_title=content_title, rv=rv)
 
 @app.route('/minkabu_fx_gbpjpy')
 def minkabu_fx_gbpjpy():
-    return 'minkabu_fx_gbpjpy'
+    rv = query_db(BACKTEST_HISTORY_QUERY.format(symbol='GBPJPY'))
+    content_title = "みん株FX GBPJPY Backtest Data"
+    return render_template('backtest_history_table.html', content_title=content_title, rv=rv)
 
 @app.route('/nikkei_topix')
 def nikkei_topix():
