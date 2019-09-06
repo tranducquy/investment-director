8a464189657d9ba2f1784e094c3e06a11fe65122
1011.prodesk.ubuntu <takeyuki.t@gmail.com>
2019-09-06 19:30:02 +0900

    注文名を取引所に合わせた

--- ordertype.py
+++ ordertype.py
@@ -7,11 +7,11 @@ class OrderType(IntEnum):
     STOP_MARKET_SHORT = 2 #逆指値成行売り
     LIMIT_LONG = 3 #指値成行買い
     LIMIT_SHORT = 4 #指値成行売り
-    CLOSE_STOP_MARKET_LONG = 5 #逆指値成行買い返済
-    CLOSE_STOP_MARKET_SHORT = 6 #逆指値売り返済
-    CLOSE_MARKET_LONG = 7 #成行買い返済
-    CLOSE_MARKET_SHORT = 8 #成行売り返済
+    CLOSE_STOP_MARKET_LONG = 5 #逆指値成行返売(longを返済)
+    CLOSE_STOP_MARKET_SHORT = 6 #逆指値成行返買(shortを返済)
+    CLOSE_MARKET_LONG = 7 #成行返売(longを返済)
+    CLOSE_MARKET_SHORT = 8 #成行返買(shortを返済)
     MARKET_LONG = 9 #成行買い
     MARKET_SHORT = 10 #成行売り
-    CLOSE_LIMIT_LONG = 11 #指値買い返済
-    CLOSE_LIMIT_SHORT = 12 #指値売り返済
+    CLOSE_LIMIT_LONG = 11 #指値返売(longを返済)
+    CLOSE_LIMIT_SHORT = 12 #指値返買(shortを返済)
