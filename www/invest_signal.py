8c8a8c45abb4077446f2adb1ecd8d952d3cce24d
ryzen.ubuntu <takeyuki.t@gmail.com>
2019-09-15 20:08:03 +0900

    カラム名変更

--- www/invest_signal.py
+++ www/invest_signal.py
@@ -17,21 +17,21 @@ def _get_open_signal_nikkei225_topix500(db, start_date, end_date, symbols):
     ,order_table.order_name
     ,order_table.order_price
     ,r.end_date
-    ,r.expected_rate_3month as 期待利益率3か月
-    ,r.long_expected_rate_3month as 期待利益率3か月long
-    ,r.short_expected_rate_3month as 期待利益率3か月short
-    ,r.expected_rate_1year as 期待利益率1年
-    ,r.long_expected_rate_1year as 期待利益率1年long
-    ,r.short_expected_rate_1year as 期待利益率1年short
-    ,r.expected_rate_3year as 期待利益率3年
-    ,r.long_expected_rate_3year as 期待利益率3年long
-    ,r.short_expected_rate_3year as 期待利益率3年short
-    ,r.expected_rate_15year as 期待利益率15年
-    ,r.long_expected_rate_15year as 期待利益率15年long
-    ,r.short_expected_rate_15year as 期待利益率15年short
-    ,r.expected_rate as 全期間期待利益率
-    ,r.long_expected_rate as 全期間期待利益率long
-    ,r.short_expected_rate as 全期間期待利益率short
+    ,r.expected_rate_3month as 利益率3か月
+    ,r.long_expected_rate_3month as 利益率3か月long
+    ,r.short_expected_rate_3month as 利益率3か月short
+    ,r.expected_rate_1year as 利益率1年
+    ,r.long_expected_rate_1year as 利益率1年long
+    ,r.short_expected_rate_1year as 利益率1年short
+    ,r.expected_rate_3year as 利益率3年
+    ,r.long_expected_rate_3year as 利益率3年long
+    ,r.short_expected_rate_3year as 利益率3年short
+    ,r.expected_rate_15year as 利益率15年
+    ,r.long_expected_rate_15year as 利益率15年long
+    ,r.short_expected_rate_15year as 利益率15年short
+    ,r.expected_rate as 全期間利益率
+    ,r.long_expected_rate as 全期間利益率long
+    ,r.short_expected_rate as 全期間利益率short
     ,r.win_rate as 勝率
     ,r.average_period_per_trade as 平均取引期間
     ,r.win_count+r.loss_count as 取引数
@@ -100,21 +100,21 @@ def _get_open_signal(db, start_date, end_date, symbols, bitmex):
     ,order_table.order_name
     ,order_table.order_price
     ,r.end_date
-    ,r.expected_rate_3month as 期待利益率3か月
-    ,r.long_expected_rate_3month as 期待利益率3か月long
-    ,r.short_expected_rate_3month as 期待利益率3か月short
-    ,r.expected_rate_1year as 期待利益率1年
-    ,r.long_expected_rate_1year as 期待利益率1年long
-    ,r.short_expected_rate_1year as 期待利益率1年short
-    ,r.expected_rate_3year as 期待利益率3年
-    ,r.long_expected_rate_3year as 期待利益率3年long
-    ,r.short_expected_rate_3year as 期待利益率3年short
-    ,-1 as 期待利益率15年
-    ,-1 as 期待利益率15年long
-    ,-1 as 期待利益率15年short
-    ,r.expected_rate as 全期間期待利益率
-    ,r.long_expected_rate as 全期間期待利益率long
-    ,r.short_expected_rate as 全期間期待利益率short
+    ,r.expected_rate_3month as 利益率3か月
+    ,r.long_expected_rate_3month as 利益率3か月long
+    ,r.short_expected_rate_3month as 利益率3か月short
+    ,r.expected_rate_1year as 利益率1年
+    ,r.long_expected_rate_1year as 利益率1年long
+    ,r.short_expected_rate_1year as 利益率1年short
+    ,r.expected_rate_3year as 利益率3年
+    ,r.long_expected_rate_3year as 利益率3年long
+    ,r.short_expected_rate_3year as 利益率3年short
+    ,-1 as 利益率15年
+    ,-1 as 利益率15年long
+    ,-1 as 利益率15年short
+    ,r.expected_rate as 全期間利益率
+    ,r.long_expected_rate as 全期間利益率long
+    ,r.short_expected_rate as 全期間利益率short
     ,r.win_rate as 勝率
     ,r.average_period_per_trade as 平均取引期間
     ,r.win_count+r.loss_count as 取引数
